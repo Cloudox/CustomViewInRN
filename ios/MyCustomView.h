0a70b86532a852bfe019056fc42b736a7763d437
Cloudox <cloudox@foxmail.com>
2016-05-23 16:46:37 +0800

    创建工程；创建自定义视图文件

---
+++ ios/MyCustomView.h
@@ -0,0 +1,13 @@
+//
+//  MyCustomView.h
+//  customViewInRN
+//
+//  Created by csdc-iMac on 16/5/23.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyCustomView : UIView
+
+@end
---
+++ ios/MyCustomView.m
@@ -0,0 +1,21 @@
+//
+//  MyCustomView.m
+//  customViewInRN
+//
+//  Created by csdc-iMac on 16/5/23.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import "MyCustomView.h"
+
+@implementation MyCustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
